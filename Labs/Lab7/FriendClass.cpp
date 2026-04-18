d375245d98ec3b5e9a7ec8bb2e606b46fd081092
Daniya_Ali <alidaniya419@gmail.com>
2026-04-18 17:02:26 +0500

    Add files via upload

---
+++ Labs/Lab7/FriendClass.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+class Employee{
+  private:
+    string name,designation;
+    int id,salary;
+ public:
+    friend class  Payroll;
+    Employee(string n="XYZ",string d="None",int i=0,int s=0) : name(n),designation(d),id(i),salary(s) {}
+    void set_info(){
+        cout<<"Enter name : ";
+        cin>>name;
+        cout<<"Enter your designation : ";
+        cin>>designation;
+        cout<<"Enter the employee id : ";
+        cin>>id;
+        cout<<"Enter salary in ruppees : ";
+        cin>>salary;
+    }
+    void get_info(){
+        cout<<"Name : "<<name<<endl;
+        cout<<"Designation : "<<designation<<endl;
+        cout<<"ID : "<<id<<endl;
+        cout<<"Salary : Rs."<<salary<<endl;
+    }
+};
+class Payroll{
+    public:
+        void IncreaseSalary(Employee& e,double IncPercent){
+            if(IncPercent>0 || IncPercent<=100){
+                double increment=e.salary*(IncPercent/100);
+                e.salary+=increment;
+                cout<<"The salary for "<<e.name<<" has been updated."<<endl;
+            }
+        }
+};
+
+int main(){
+    Employee e1;
+    e1.set_info();
+    cout<<"The record of the employee before increment is : "<<endl;
+    e1.get_info();
+    cout<<"-------------------------------------------------"<<endl;
+    Payroll p1;
+    p1.IncreaseSalary(e1,24.5);
+    cout<<"-------------------------------------------------"<<endl;
+    cout<<"The record of the employee after increment is : "<<endl;
+    e1.get_info();
+    
+    return 0;
+}
